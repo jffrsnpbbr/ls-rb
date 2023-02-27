cdee8383668279bf4eef526984c88c05b31ba483
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-02-27 11:31:31 +0800

    flow-control#comparisons

---
+++ flow-control/comparisons.rb
@@ -0,0 +1,38 @@
+# == is equal to operator
+5 == 5
+5 == 6
+'abc' == 'abc'
+'abc' == 'aBc'
+'5' == '5'
+'5' == '6'
+'5' == 5
+
+# != not equal to
+
+4 != 5
+4 != 4
+4 != 156
+'abc' != 'def'
+'abc' != 'aBc'
+'5' != 5
+
+# < less than
+# > greater than
+
+4 < 5
+5 > 5
+"4" < "5"
+"4" > "5"
+"42" > "402"
+"42" < "420"
+"42" < 420
+42 > "320"
+
+# <= less than or equal to
+# >= greater than or equal to
+
+4 <= 5
+5 >= 5
+4 >= 5
+4 >= 3
+4 >= 4
